58ab25c6673ca6203b9cb7a76004d6e98fc5301b
Deniz Dilsiz <d.dilsiz@student.unsw.edu.au>
2024-04-02 11:20:42 +1100

    Pushing some initial skeleton code.

---
+++ algorithm.py
@@ -0,0 +1,38 @@
+
+def readInput():
+	#function to read in stdin and populate graph object
+	#graph object: adjacency list 
+	#Dictionary, key : vertex, value List<vertex> 
+	print("Not Implemented")
+
+def graph_addVertex():
+	#add vertex key to dict
+	print("Not Implemented")
+
+def graph_addEdge(): 
+	#add vertex v1 to dict[v2]
+	#add vertex v2 to dict[v1]
+	print("Not Implemented")
+
+def graph_deleteVertex():
+	#delete key v1
+	#delete v1 for all v2 such that dict[v2].contains(v1)
+	print("Not Implemented")
+
+def graph_deleteEdge():
+	#dict[v1].remove(v2)
+	#dict[v2].remove(v1)
+	print("Not Implemented")
+
+def execKernal():
+	#to execute our Simplification/Halting rules here. 
+	print("Not Implemented")
+
+def execSolver():
+	print("Not Implemented")
+
+
+#Main
+readInput()
+execKernal()
+execSolver()
