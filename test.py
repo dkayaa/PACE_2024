8fe92a7f66388608c668a3d803cf4220bccc9242
Deniz Dilsiz <d.dilsiz@student.unsw.edu.au>
2024-04-04 11:13:57 +1100

    additional test

--- test.py
+++ test.py
@@ -7,10 +7,16 @@ import algorithm
 #test cases here
 #-------------------------------------#
 class TestMinimise(unittest.TestCase):
+
 	def test_minimise_1(self):
 		c = lambda k : k >= 15
 		val = algorithm.minimise({},20, c);
 		self.assertEqual(val, 15, "minimise returned: " + str(val))
+
+	def test_minimise_2(self):
+		c = lambda k : k >= 5
+		val = algorithm.minimise({},20, c);
+		self.assertEqual(val, 5, "minimise returned: " + str(val))
 
 #add more here
 #-------------------------------------#
