d1a1a408a6bfb8db2373d29acf6ec870c832bee1
Sankalpa Tripathee <sankalpa.tripathee@gmail.com>
2024-04-12 15:23:40 +1000

    Deleted comments

--- bruteforce.py
+++ bruteforce.py
@@ -62,28 +62,3 @@ def BruteForceRecursing(po, k, V_2, c):
 	# else 
 	#recurse on a > b
 	return False
-
-""""
-	for a in range(n):
-		for b in range(a+1, n):
-			if(a == b):
-				continue
-			v_a = V_2[a]
-			v_b = V_2[b]
-			if (incomparable(po, v_a, v_b)):
-				po_a = copy.deepcopy(po)
-				ka = helper.commitPartialOrdering(po_a, v_a, v_b, k, c, V_2)
-				if ka >= 0 and BruteForceRecursing(po_a, ka, V_2, c):
-					po.clear()
-					po.update(po_a)
-					return True
-				po_a.clear()
-
-				po_b = copy.deepcopy(po)
-				kb = helper.commitPartialOrdering(po_b, v_b, v_a, k, c, V_2)
-				if kb >= 0 and BruteForceRecursing(po_b, kb, V_2, c):
-					po.clear()
-					po.update(po_b)
-					return True 
-				po_b.clear()
-				"""		
